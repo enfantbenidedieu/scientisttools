0ccb91d846c089811b56a4e28ec15e6aa878a3fa
Duverier <duverierdjifack@gmail.com>
2023-04-27 23:39:37 +0200

    init

--- scientisttools/manifold/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from .classicmds import CMDSCALE
-from .smacof import SMACOF
-from .mds import MDS
-
-__version__ = "0.0.2"
--- scientisttools/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from .base import *
-
-__version__ = "0.0.2"
