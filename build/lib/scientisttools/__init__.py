642dde797cc8902e1fcb325bc7cec091ce6fea15
Duverier <duverierdjifack@gmail.com>
2023-06-17 13:50:43 +0200

    update

--- build/lib/scientisttools/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from scientisttools.version import __version__
-
-__name__ = "scientisttools"
-__author__ = 'Duverier DJIFACK ZEBAZE'
-__email__ = 'duverierdjifack@gmail.com'
