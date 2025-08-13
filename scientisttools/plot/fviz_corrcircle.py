b3693f1dfa28a44d47115bb4b07b55c08d8d9628
Duverier <duverierdjifack@gmail.com>
2025-08-13 23:35:08 +0200

    Update fviz_corrcircle

--- scientisttools/plot/fviz_corrcircle.py
+++ scientisttools/plot/fviz_corrcircle.py
@@ -1,50 +1,31 @@
 # -*- coding: utf-8 -*-
-from plotnine import ggplot,aes,geom_segment,annotate,arrow,theme_minimal
-from numpy import asarray
+from plotnine import ggplot, arrow, theme_minimal
 
 #intern functions
-from .fviz_add import text_label,fviz_add,gg_circle
+from .fviz import add_arrow, set_axis, fviz_circle
 
 def fviz_corrcircle(self,
                     axis = [0,1],
-                    geom = ["arrow","text"],
+                    geom = ("arrow","text"),
+                    repel = False,
+                    col_var = "black",
+                    segment_args_var = dict(linetype="solid",size=0.5,arrow = arrow(angle=10,length=0.1,type="closed")),
+                    text_args_var = dict(size=8),
+                    col_quanti_sup = "blue",
+                    segment_args_quanti_sup = dict(linetype="dashed",size=0.5,arrow = arrow(angle=10,length=0.1,type="closed")),
+                    text_args_quanti_sup = dict(size=8),
+                    scale = 1,
+                    add_circle = True,
+                    col_circle = "gray",
+                    x_lim = (-1.1,1.1),
+                    y_lim = (-1.1,1.1),
                     x_label = None,
                     y_label = None,
                     title = None,
-                    alpha_var = 1,
-                    col_var = "black",
-                    linetype_var = "solid",
-                    line_size_var = 0.5,
-                    arrow_length_var =0.1,
-                    arrow_angle_var = 10,
-                    arrow_type_var = "closed",
-                    text_type_var = "text",
-                    text_size_var = 8,
-                    alpha_quanti_sup = 1,
-                    col_quanti_sup = "blue",
-                    linetype_quanti_sup = "dashed",
-                    line_size_quanti_sup = 0.5,
-                    arrow_length_quanti_sup =0.1,
-                    arrow_angle_quanti_sup = 10,
-                    arrow_type_quanti_sup = "closed",
-                    text_type_quanti_sup = "text",
-                    text_size_quanti_sup = 8,
-                    add_circle = True,
-                    col_circle = "gray",
-                    add_grid=True,
                     add_hline = True,
-                    alpha_hline = 0.5,
-                    col_hline = "black",
-                    size_hline = 0.5,
-                    linetype_hline = "dashed",
                     add_vline = True,
-                    alpha_vline = 0.5,
-                    col_vline = "black",
-                    size_vline = 0.5,
-                    linetype_vline = "dashed",
-                    ha_var = "center",
-                    va_var = "center",
-                    ggtheme=theme_minimal()):
+                    add_grid = True,
+                    ggtheme = theme_minimal()):
     """
     Draw correlation circle
     -----------------------
@@ -52,6 +33,17 @@ def fviz_corrcircle(self,
     Description
     -----------
 
+    Usage
+    -----
+    ```python
+    >>> fviz_corrcircle(self,**kwargs)
+    ```
+
+    Parameters
+    ----------
+    `self`: an object of class PCA, PartiaLPCA, CA, MCA, FAMD
+
+    `**kwargs`: additionals informations. For more see `fviz_arrow`.
 
     Return
     ------
@@ -60,14 +52,24 @@ def fviz_corrcircle(self,
     Author(s)
     ---------
     Duvérier DJIFACK ZEBAZE djifacklab@gmail.com
+
+    Examples
+    --------
+    ```python
+    >>> from scientisttools import decathlon, PCA, fviz_corrcircle
+    >>> res_pca = PCA(ind_sup=(41,42,43,44,45),quanti_sup=(10,11),quali_sup=12,rotate=None)
+    >>> res_pca.fit(decathlon)
+    >>> #graph of variables
+    >>> print(fviz_corrcircle(res_pca))
+
     """
-    if self.model_ not in ["pca","ca","mca","specificmca","famd","mpca","pcamix","mfa","mfaqual","mfamix","partialpca","efa"]:
-        raise TypeError("'self' must be an object of class PCA, CA, MCA, SpecificMCA, FAMD, MPCA, PCAMIX, MFA, MFAQUAL, MFAMIX, PartialPCA, EFA")
+    if self.model_ not in ["pca","partialpca","fa","ca","mca","specificmca","famd","mpca","pcamix","mfa","mfaqual","mfamix"]:
+        raise TypeError("'self' must be an object of class PCA, PartialPCA, FactorAnalysis, CA, MCA, SpecificMCA, FAMD, MPCA, PCAMIX, MFA, MFAQUAL, MFAMIX")
     
     if ((len(axis) !=2) or (axis[0] < 0) or (axis[1] > self.call_.n_components-1)  or (axis[0] > axis[1])) :
         raise ValueError("You must pass a valid 'axis'.")
     
-    if self.model_ in ["pca","partialpca","efa"]:
+    if self.model_ in ["pca","partialpca","fa"]:
         coord = self.var_.coord
     elif self.model_ in ["famd","mpca","pcamix","mfa","mfamix"]:
         coord = self.quanti_var_.coord
@@ -77,35 +79,24 @@ def fviz_corrcircle(self,
         if hasattr(self, "quanti_var_sup_"):
             coord = self.quanti_var_sup_.coord
 
-    # Initialize
-    p = ggplot(data=coord,mapping=aes(x = f"Dim.{axis[0]+1}",y=f"Dim.{axis[1]+1}",label=coord.index))
-
-    if "arrow" in geom:
-        p = p + geom_segment(aes(x=0,y=0,xend=f"Dim.{axis[0]+1}",yend=f"Dim.{axis[1]+1}"),alpha=alpha_var,color=col_var,linetype=linetype_var,size=line_size_var,arrow = arrow(length=arrow_length_var,angle=arrow_angle_var,type=arrow_type_var))
-    if "text" in geom:
-            p = p + text_label(text_type_var,False,color=col_var,size=text_size_var,ha=ha_var,va=va_var)
-        
+    #initialize
+    p = add_arrow(p=ggplot(),data=coord.mul(scale),axis=axis,geom=geom,repel=repel,color=col_var,segment_args=segment_args_var,text_args=text_args_var)
+            
     if self.model_ in ["pca","famd","mpca","pcamix","mfa","mfamix"]:
         if hasattr(self, "quanti_sup_"):
             sup_coord = self.quanti_sup_.coord
         elif hasattr(self, "quanti_var_sup_"):
             sup_coord = self.quanti_var_sup_.coord
-        
         if hasattr(self, "quanti_sup_") or hasattr(self, "quanti_var_sup_"):
-            if "arrow" in geom:
-                p  = p + annotate("segment",x=0,y=0,xend=asarray(sup_coord.iloc[:,axis[0]]),yend=asarray(sup_coord.iloc[:,axis[1]]),alpha=alpha_quanti_sup,color=col_quanti_sup,linetype=linetype_quanti_sup,size=line_size_quanti_sup,arrow = arrow(length=arrow_length_quanti_sup,angle=arrow_angle_quanti_sup,type=arrow_type_quanti_sup))
-            if "text" in geom:
-                p  = p + text_label(text_type_quanti_sup,False,data=sup_coord,mapping=aes(x = f"Dim.{axis[0]+1}",y=f"Dim.{axis[1]+1}",label=sup_coord.index),color=col_quanti_sup,size=text_size_quanti_sup,ha=ha_var,va=va_var)
+            p = add_arrow(p=p,data=sup_coord.mul(scale),axis=axis,geom=geom,repel=repel,color=col_quanti_sup,segment_args=segment_args_quanti_sup,text_args=text_args_quanti_sup)
     
-    # Create circle
+    #add correlation circle
     if add_circle:
-        p = p + gg_circle(r=1.0, xc=0.0, yc=0.0, color=col_circle, fill=None)
+        p = fviz_circle(p=p,color=col_circle)
 
-    #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    ##add additionnal informations
-    #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #add others elements
     if title is None:
         title = "Correlation circle"
-    p = fviz_add(p,self,axis,x_label,y_label,title,(-1,1),(-1,1),add_hline,alpha_hline,col_hline,linetype_hline,size_hline,add_vline,alpha_vline,col_vline,linetype_vline,size_vline,add_grid,ggtheme)     
-    
+    p = set_axis(p=p,self=self,axis=axis,x_lim=x_lim,y_lim=y_lim,x_label=x_label,y_label=y_label,title=title,add_hline=add_hline,add_vline=add_vline,add_grid=add_grid,ggtheme=ggtheme)
+      
     return p
