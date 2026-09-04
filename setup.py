e51d6a7fd86a11781d94a851407d6b9d6657649d
duverier <duverierdjifack@gmail.com>
2026-09-04 21:13:16 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,37 +1,46 @@
-import setuptools
-
-with open("README.md", "r",encoding="utf-8") as fh:
-    long_description = fh.read()
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
 # Setting up
-setuptools.setup(
-    name="scientisttools",
-    version="0.1.6",
-    author="Duverier DJIFACK ZEBAZE",
-    author_email="djifacklab@gmail.com",
-    description="Python library for multidimensional analysis, classification - clustering analysis and multidimensional analysis",
-    long_description_content_type="text/markdown",
-    long_description=long_description,
-    packages=setuptools.find_packages(),
-    install_requires=["numpy>=1.26.4",
-                      "matplotlib>=3.8.4",
-                      "scikit-learn>=1.2.2",
-                      "pandas>=2.2.2",
-                      "polars>=0.19.2",
-                      "mapply>=0.1.21",
-                      "plotnine>=0.10.1",
-                      "pingouin>=0.5.4",
-                      "scientistmetrics>=0.0.4"],
-    python_requires=">=3.10",
-    include_package_data=True,
-    package_data={"": ["data/*.xlsx",
-                       "data/*.xls",
-                       "data/*.txt",
-                       "data/*.csv",
-                       "data/*.rda"]},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+setup(
+    name = "scientisttools",
+    version = "0.2.0",
+    author = "Duverier DJIFACK ZEBAZE",
+    author_email = "djifacklab@gmail.com",
+    description = "Python library for multidimensional analysis, classification - clustering analysis and multidimensional analysis",
+    long_description_content_type = "text/markdown",
+    long_description = open("README.md", "r",encoding="utf-8").read(),
+    url = "https://github.com/enfantbenidedieu/scientisttools",
+    packages = find_packages(),
+    classifiers = [
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+        ],
+    python_requires = ">=3.11",
+    install_requires = [
+            "numpy>=1.21",
+            "pandas>=1.4",
+            "scikit-learn>=1.2",
+            "statsmodels>=0.14.6",
+            "plotnine>=0.10.1",
+            "openpyxl>=3.1.5",
+            "adjustText>=0.8.2",
+            "pyreadr>=0.5.4",
+            "mizani>=0.14.4",
+            "tabulate>=0.9.0"
+        ],
+    include_package_data = True,
+    package_data = {"": ["data/"]},
+    keywords = "multidimensional analysis, clustering analysis, data visualization, statistical analysis, data science, machine learning",
+    project_urls = {
+        "Bug Reports": "https://github.com/enfantbenidedieu/scientisttools/issues",
+        "Source": "https://github.com/enfantbenidedieu/scientisttools",
+        "Documentation": "https://scientisttools.readthedocs.io",
+    }
 )
