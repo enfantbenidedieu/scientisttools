060fb96d8ccc41dd2c353382d3aa7a8e5cc73167
duverier <duverierdjifack@gmail.com>
2026-09-06 19:50:37 +0200

    update setup and init files

--- scientisttools/__init__.py
+++ scientisttools/__init__.py
@@ -5,7 +5,7 @@ from .extraction import *
 from .methods import *
 from .plot import *
 
-__version__ = '0.2.0'
+__version__ = '0.2.0post1'
 __name__ = "scientisttools"
 __author__ = 'Duverier DJIFACK ZEBAZE'
 __email__ = 'djifacklab@gmail.com'
